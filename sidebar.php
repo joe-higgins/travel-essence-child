b528e508fd94b15f9b6fbc3f3f3aed4bbd4dce2c
Joe Higgins <joehiggins.bass@gmail.com>
2019-02-05 13:42:04 -0500

    Added new image to sidebar

--- sidebar.php
+++ sidebar.php
@@ -21,7 +21,7 @@ if ( ! defined( 'WPINC' ) ) {
 ?>
 <!-- WP sidebar -->
 <section id="caitlin-statement">
-  <div id="caitlin"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/caitlin.png" alt="Caitlin" /></div>
+  <div id="caitlin"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/caitlin-darren.jpg" alt="Caitlin" /></div>
   <div id="statement"><p>We travel the world, discovered and undiscovered, near and far…. We carry with us our interests and passions, which are ever-enhanced by global ideas and perspectives.  This blog is devoted to our inspirations and discoveries from around the world, from wine and fashion to wellness and experiences, and more in between. Please read and share the essence of our travels.</p></div>
 </section>
 <!-- end wp sidebar -->

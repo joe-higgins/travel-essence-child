94f400dee447601ed671ed4f75bd28cf1cf529af
Joe Higgins <joehiggins.bass@gmail.com>
2019-02-14 11:26:45 -0500

    Added category titles to cat page

--- category.php
+++ category.php
@@ -20,6 +20,7 @@
 get_header(); ?>
 <?php get_sidebar(); ?>
 <div id="blog-menu">
+  <h1 id="category-title" class="scriptina-cat-heading"><?php single_cat_title(); ?></h1>
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
       <!-- contents of the loop -->
 

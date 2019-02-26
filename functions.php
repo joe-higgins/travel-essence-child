8623593e44f38a8d12c643ac4e9a39bd1c1402cd
Joe Higgins <joehiggins.bass@gmail.com>
2019-02-25 23:05:51 -0500

    Registered widget area for left sidebar below statement

--- functions.php
+++ functions.php
@@ -64,5 +64,23 @@ function travel_essence_setup() {
 }
 add_action( 'after_setup_theme', 'travel_essence_setup' );
 
+/**
+ * Register our sidebars and widgetized areas.
+ *
+ */
+function arphabet_widgets_init() {
+
+	register_sidebar( array(
+		'name'          => 'Left Sidebar',
+		'id'            => 'home_left',
+		'before_widget' => '<div>',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h2 class="rounded">',
+		'after_title'   => '</h2>',
+	) );
+
+}
+add_action( 'widgets_init', 'arphabet_widgets_init' );
+
 // No php closing tag in other themes.  Don't know why just yet:-)
 // 1/28/19
--- sidebar.php
+++ sidebar.php
@@ -20,8 +20,16 @@ if ( ! defined( 'WPINC' ) ) {
 }
 ?>
 <!-- WP sidebar -->
+<?php if ( is_active_sidebar( 'home_left' ) ) : ?>
+	<div id="primary-sidebar" class="primary-sidebar widget-area" role="complementary">
+		<?php dynamic_sidebar( 'home_left' ); ?>
+	</div><!-- #primary-sidebar -->
+<?php endif; ?>
 <section id="caitlin-statement">
   <div id="caitlin"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/caitlin-darren.jpg" alt="Caitlin" /></div>
-  <div id="statement"><p>We travel the world, discovered and undiscovered, near and far…. We carry with us our interests and passions, which are ever-enhanced by global ideas and perspectives.  This blog is devoted to our inspirations and discoveries from around the world, from wine and fashion to wellness and experiences, and more in between. Please read and share the essence of our travels.</p></div>
+  <div id="statement">
+		<p>We travel the world, discovered and undiscovered, near and far…. We carry with us our interests and passions, which are ever-enhanced by global ideas and perspectives.  This blog is devoted to our inspirations and discoveries from around the world, from wine and fashion to wellness and experiences, and more in between. Please read and share the essence of our travels.</p>
+		<?php dynamic_sidebar( 'home_left' ); ?>
+	</div>
 </section>
 <!-- end wp sidebar -->
